429014a6e3cfefd26a68eb5c7890c443e1f5869a
ms198 <muhamadsobari198@gmail.com>
2022-11-03 15:52:34 +0700

    constans colors etch

---
+++ src/constants/categoy.js
@@ -0,0 +1,3 @@
+const category = [ { id: '1', name: 'Komputer & Latop' }, { id: '2', name: 'CCTV' }, { id: '3', name: 'AC' } ];
+
+export default category;
---
+++ src/constants/colors.js
@@ -0,0 +1,13 @@
+const COLORS = {
+	white: '#FFF',
+	dark: '#000',
+	primary: '#52b1ea',
+	secondary: '#F5BA46',
+	light: '#d2d2d2',
+	light2: '#aaaaaa',
+	light3: '#eeeeee',
+	grey: '#686C71',
+	red: 'red'
+};
+
+export default COLORS;
